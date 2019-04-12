18450cf138026b5670c871247a19e15d99856de7
Andrea de Anda <adaku.andy@gmail.com>
2019-04-12 16:18:41 -0500

    js-ejercico3

--- Javascript/ejercicio3/javascript/main.js
+++ Javascript/ejercicio3/javascript/main.js
@@ -1,32 +1,102 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 //var httpChannel = subject.
-
+var data;
+var arrayIdiomas = []
+var costo=0;
+var niveles=[];
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://apiidiomas.firebaseapp.com/idiomas.json', true);
 
 request.onload = function () {
-  var data = JSON.parse(this.response);
-
-  console.log(typeof (data));
+   data = JSON.parse(this.response);
   //console.log(data);
-
   if (request.status >= 200 && request.status < 400) {
     var selectIdiomas = document.getElementById("idiomas");
-    var option;
-    var idiomas = Object.keys(data);
+    //el object.keys nos da los nombres de los idiomas
+    var nomIdiomas = Object.keys(data);
+
+    for (const key in data) {
+      if (data.hasOwnProperty(key)) {
+        var option = document.createElement("option");
+        option.text = key;
+        selectIdiomas.add(option,selectIdiomas[nomIdiomas.length]);
+        //console.log(key);
+       arrayIdiomas.push({
+         idioma: key,
+         ...data[key]
+       })
+      }
+    }
+    //
     
-    idiomas.forEach(elem => {
-      var selectIdiomas = document.getElementById("idiomas");
-      var option = document.createElement("option");
-      option.text = elem;
-      selectIdiomas.add(option,selectIdiomas[0]);
-    })
+
   } else
     console.log('error');
-  
-  
 }
 
 // Send request
 request.send();
+
+function myFunction() {
+  var selIdioma = document.getElementById("idiomas");
+  var txt= selIdioma.options[selIdioma.selectedIndex].text;
+
+  crearDiv(txt);
+
+}
+
+function crearDiv(txt){
+  var desc;
+  console.log(txt);
+
+  //console.log(arrayIdiomas);
+  var idioma= arrayIdiomas.find(obj => obj.idioma == txt);
+  //console.log(idioma.niveles);
+  desc=idioma.descuento.substr(0,2);
+  desc=100-Number.parseInt(desc);
+  //console.log(desc);
+
+
+  //crear un div 
+  var myDiv = document.createElement("div");
+  myDiv.innerHTML="Idioma: "+txt+"<br>"+"Precio:  "+ idioma.precio+"<br>"+"Profesor:  "+ idioma.profesor+"<br>"+ "Descuento:  "+ idioma.descuento +"<br>"+ "Niveles:  ";
+
+  var sel = document.createElement("select");
+  llenarNiveles(sel, idioma.niveles);
+
+
+  //generar id
+  var uniqueId = 'id-' + Math.random().toString(36).substr(2, 16)+txt;
+  var id= document.createTextNode(uniqueId);
+
+  myDiv.appendChild(sel);
+  
+  myDiv.id=uniqueId;
+  //darle formato al div
+  myDiv.style.width="200px";
+  myDiv.style.display="block";
+  myDiv.style.textAlign="center";
+  myDiv.style.margin=20;
+  //myDiv.style.color="#0000CD";
+  
+  document.getElementsByClassName("aux")[0].appendChild(myDiv);   
+  //para el click del boton   
+
+  
+  costo+= ((Number.parseInt(idioma.precio))*desc)/100;
+  //console.log(costo);
+}  
+
+function llenarNiveles(select, array){
+  var a= array.keys();
+
+  for(i=0; i<array.length; i++){
+    var option = document.createElement("option");
+    option.text =array[i];
+    select.add(option,select[a.length]);
+  }
+
+}
+
+
--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,87 +1,40 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-container grid-list-sm fluid>
+          <v-layout row wrap>
+            <v-flex
+              v-for="n in 18"
+              :key="n"
+              xs4
+              d-flex
+            >
+              <v-card flat tile class="d-flex">
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                  aspect-ratio="2"
+                  class="grey lighten-2"
+                >
+                  <template v-slot:placeholder>
+                    <v-layout
+                      fill-height
+                      align-center
+                      justify-center
+                      ma-0
+                    >
+                      <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                    </v-layout>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
